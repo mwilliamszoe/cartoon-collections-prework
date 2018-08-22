8cc441d2113367cec9fcce6d02fb1093e09f2906
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 16:18:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,11 @@ def roll_call_dwarves(array)
   end 
 end 
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(array)
+  array.collect do |word| 
+    word.capitalize + "!"
+  end 
+end 
 
 def long_planeteer_calls# code an argument here
   # Your code here
