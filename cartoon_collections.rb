875b1b57f1493b9146db13e99c56fd1fc59295df
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 16:15:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves(dwarves)
-  # Your code here
-end
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end 
+end 
 
 def summon_captain_planet# code an argument here
   # Your code here
